52723266063b418455641dc82d09593d3feb0488
JenniaGo <jenniagorelik@gmail.com>
2023-01-20 20:46:39 +0200

    Update views.py

---
+++ app/games/memorygame/views.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from .models import MemoryGame, Score
+from .forms import MemoryForm
+import random
+
+app = Flask(__name__)
+
+@app.route('/memorygame', methods=['GET', 'POST'])
+def memorygame():
+    form = MemoryForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            selected_cards = form.selected_cards.data
+            secret_cards = request.cookies.get('secret_cards')
+            if selected_cards == secret_cards:
+                flash('Congratulations! You have matched all the cards.', 'success')
+                score = Score.query.filter_by(user_id=request.cookies.get('user_id')).first()
+                score.score += 1
+                score.update()
+                secret_cards = random.sample(range(1,9), 4)
+                return redirect(url_for('memorygame'))
+            else:
+                flash('Sorry! The cards you have selected are not matching. Please try again.', 'danger')
+        else:
+            flash('Invalid Input', 'danger')
+    secret_cards = random.sample(range(1,9), 4)
+    resp = make_response(render_template('memorygame.html', form=form, cards=range(1,9)))
+    resp.set_cookie('secret_cards', str(secret_cards))
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
