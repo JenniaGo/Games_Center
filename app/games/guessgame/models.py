12d50fd0f1e833bed08ff21ebebfa0d978ec2c96
JenniaGo <jenniagorelik@gmail.com>
2023-01-20 20:39:33 +0200

    Update models.py

---
+++ app/games/guessgame/models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import INTEGER
+
+db = SQLAlchemy()
+
+class GuessGame(db.Model):
+    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255))
+    score = db.Column(db.Integer)
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+class Score(db.Model):
+    id = db.Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    user_id = db.Column(db.String(255))
+    score = db.Column(db.Integer)
+
+    def __init__(self, user_id, score):
+        self.user_id = user_id
+        self.score = score
+
+    def update(self):
+        db.session.commit()
